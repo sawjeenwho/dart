f41a8733cce2b7ee9f15a440201e76911440043d
s.savin <s.savin@vk.team>
2024-04-29 11:44:16 +0300

    Класс, метод

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,7 +1,31 @@
-var num1 = 5;
 void main() {
-  var kek = [];
-  kek.addAll([1, 2, 3, 4, 5, 6]);
-  kek.removeWhere((element) => element >= 5);
-  print(kek);
+  var sergo = User('Sergey', 25, true, ['PC']);
+  var leha = User('Alexey', 25, true, ['Sex']);
+  sergo.info();
+  leha.info();
+}
+
+class User {
+  // Описание класса
+  late String name;
+  late int age;
+  late bool isHappy;
+  late List<String> hobbies;
+
+  User(name, [age, isHappy, hobbies]) {
+    // конструктор, который передаем в класс
+    this.name = name;
+    this.age = age;
+    this.isHappy = isHappy;
+    this.hobbies = hobbies;
+  }
+
+  void info() {
+    // функция вывода информации о юзере
+    var happy = isHappy ? 'happy' : 'not happy';
+    print('He name is $name, $age years old. He $happy. His hobbies:');
+    for (var el in hobbies) {
+      print(el);
+    }
+  }
 }
